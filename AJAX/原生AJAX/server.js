1e86eb803361f03318f80ee0de493077dab8261f
Duye <1939034592@qq.com>
2021-01-11 19:15:19 +0800

    learn

---
+++ AJAX/原生AJAX/server.js
@@ -0,0 +1,30 @@
+// 1.引用express
+const express = require('express');
+// 2.创建应用对象
+const app = express();
+
+// 3.创建路由规则
+// request 是对请求报文的封装
+// response 是对响应报文的封装
+app.get('/server',(request,response)=>{
+    // 设置响应头
+    response.setHeader('Access-Control-Allow-Origin','*');
+
+    // 设置响应
+    // response.send('Hello EXPRESS');
+    response.send("HELLO AJAX")
+});
+app.post('/server',(request,response)=>{
+    // 设置响应头
+    response.setHeader('Access-Control-Allow-Origin','*');
+
+    // 设置响应
+    // response.send('Hello EXPRESS');
+    response.send("HELLO AJAX POST")
+});
+
+// 4.监听端口启动服务
+app.listen(8000,()=>{
+    console.log("服务已启动，8000 端口监听中")
+    
+})
